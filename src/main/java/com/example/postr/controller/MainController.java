1ac18c7be4312cc9fd70143722afcd69029f95ff
viczaycev <dankmemes2k1@gmail.com>
2022-05-16 18:39:17 +0300

    Spring Boot Jpa. Добавлены связи между таблицами в БД. Добавлен пользователь, создавший сообщение

--- src/main/java/com/example/postr/controller/MainController.java
+++ src/main/java/com/example/postr/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.postr.controller;
 
 import com.example.postr.domain.Message;
+import com.example.postr.domain.User;
 import com.example.postr.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +31,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
